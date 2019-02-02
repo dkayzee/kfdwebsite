7ae1b2c4287eb91d12f0ff56769bd5f08973886b
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-02-02 13:17:53 -0500

    first commit, set a header base

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div class='header'><h1>Icon</h1></div>
+
+      <div class='header'>
+        <h1>Nav</h1>
+        <ul>
+          <li>WELCOME</li>
+          <li>MENU</li>
+          <li>EVENTS</li>
+          <li>GALLERY</li>
+          <li>MEDIA EXPOSURE</li>
+          <li>LOCATIONS</li>
+          <li>ORDER ONLINE</li>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export default Header
